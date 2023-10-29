bea0d2e1a17676149edc16bce20ac011284daba3
Vadim Potekhin <potekhinvadim@ya.ru>
2023-10-29 13:38:17 +0300

    Initial commit

---
+++ stations/views.py
@@ -0,0 +1,27 @@
+import csv
+
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+with open('data-398-2018-08-30.csv', encoding='utf-8') as database:
+    reader = csv.DictReader(database)
+    CONTENT = []
+    for row in reader:
+        station = {'Name': row['Name'], 'Street': row['Street'], 'District': row['District']}
+        CONTENT.append(station)
+
+
+def index(request):
+    return redirect(reverse('bus_stations'))
+
+
+def bus_stations(request):
+    page_num = int(request.GET.get('page', 1))
+    paginator = Paginator(CONTENT, 10)
+    page = paginator.get_page(page_num)
+    context = {
+        'bus_stations': page.object_list,
+        'page': page,
+    }
+    return render(request, 'stations/index.html', context)
